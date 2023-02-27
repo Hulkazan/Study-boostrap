d36036929265989803c03c733811b8a1087d4fda
Amir Gubeev <hulkazan345@gmail.com>
2023-02-27 11:47:53 +0300

    initail

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+let burger = document.querySelector('.burger');
+let header = document.querySelector('.header__content');
+
+burger.addEventListener('click', function () {
+  header.classList.toggle('open');
+} )
+
